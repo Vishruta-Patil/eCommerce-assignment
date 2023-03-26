6e496e61f7dbe4dcab1947596685f9f7d5892a46
Vishruta-Patil <vishrutapatil2106@gmail.com>
2023-03-26 11:36:10 +0530

    feat: cantrollers, model, routes of backend

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const connectDB = require("./db/db.connect")
+const pageNotFound = require("./middleware/pageNotFound")
+const cors = require("cors")
+
+const product = require("./routes/product.routes")
+const category = require('./routes/category.routes')
+const auth = require('./routes/auth.routes')
+const cart = require('./routes/cart.routes')
+const wishlist = require('./routes/wishlist.routes')
+const corsOptions = require("./utils/corsConfig")
+
+const app = express()
+app.use(express.json())
+app.use(cors(corsOptions))
+
+connectDB()
+
+const port = 8000
+
+app.get("/", (req,res) => {
+    res.send("Server Started on the PORT")
+})
+
+// routes middleware
+app.use("/products", product)
+app.use("/categories", category)
+app.use("/", auth)
+app.use("/cart", cart)
+app.use("/wishlist", wishlist)
+
+app.use(pageNotFound)
+
+app.listen(port, () => {
+    console.log("server started")
+})
